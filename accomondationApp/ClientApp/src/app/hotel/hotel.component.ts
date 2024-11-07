ffa246573ae63e80b869ae98193a56e1a35a6970
Schmidt Mihaly <smiti6812@gmail.com>
2024-11-07 21:05:44 +0100

    fixing hotel  componenet

--- accomondationApp/ClientApp/src/app/hotel/hotel.component.ts
+++ accomondationApp/ClientApp/src/app/hotel/hotel.component.ts
@@ -60,13 +60,12 @@ export class HotelComponent {
     this.updateReservedSavedFromReservation();
   }
 
-  focusSearch(){
-    setTimeout(()=>{
+  focusSearch() {
+    setTimeout(() => {
       this.searchElement.nativeElement.focus();
-    },0);
+    }, 0);
   }
-
-  cancelForm(e: any){
+  cancelForm(e: any) {
     this.showModal = false;
     this.start = -1;
     this.end = -1;
@@ -77,28 +76,28 @@ export class HotelComponent {
     this.focusSearch();
   }
 
-  getReservationForm(form: ReservationForm){
-    let res: Reservation = <Reservation> {
+  getReservationForm(form: ReservationForm) {
+    let res: Reservation = <Reservation>{
       reservedRoom: form.view.room,
       name: form.name,
       email: form.email,
       startDate: form.startDate,
       endDate: form.endDate
     };
-    for(let i = this.start; i <= this.end; i++){
-        if (!this.selected.find(val => val === form.view.dayDates[i])){
-          this.selected.push(form.view.dayDates[i]);
-        }
-        let row = this.reservationView.indexOf(form.view);
-        this.selectedDateArr[row][i]= true;
+    for (let i = this.start; i <= this.end; i++) {
+      if (!this.selected.find(val => val === form.view.dayDates[i])) {
+        this.selected.push(form.view.dayDates[i]);
+      }
+      let row = this.reservationView.indexOf(form.view);
+      this.selectedDateArr[row][i] = true;
     }
-  
+
     form.view.reservationSaved[this.end] = true;
-    form.view.reservationStartSaved[this.start -1] = true;
-    if ( this.selected.length > 1){
+    form.view.reservationStartSaved[this.start - 1] = true;
+    if (this.selected.length > 1) {
       form.view.reservationName[this.start] = res.name;
     }
-    else{
+    else {
       form.view.reservationName[this.start - 1] = res.name;
     }
     this.res = res;
@@ -109,7 +108,7 @@ export class HotelComponent {
     this.selectedRow = -1;
   }
 
-  setSortParams(param: any){    
+  setSortParams(param: any) {
     this.direction = param.dir;
     this.column = param.col;
     this.type = param.typ;
@@ -117,215 +116,214 @@ export class HotelComponent {
     this.updateReservedSavedFromReservation();
   }
 
-  onMouseArrDown(row : number, col : number, date: DateTime, roomNumber: string){
-      if (!this.checkReservation1(roomNumber, date)){
+  onMouseArrDown(row: number, col: number, date: DateTime, roomNumber: string) {
+    if (!this.checkReservation1(roomNumber, date)) {
+      this.clearSelectedDateArr();
+      this.selected.push(date);
+      this.selectedDateArr[row][col] = true;
+      this.start = col + 1;
+      this.selectedRow = row;
+    }
+  }
+
+  onMouseArrOver(row: number, col: number, date: DateTime, roomNumber: string) {
+    if (this.start > -1 && !this.checkReservation1(roomNumber, date)) {
+      if (this.selectedRow === row) {
+        for (let i = this.start; i <= col; i++) {
+          this.selectedDateArr[row][col] = true;
+        }
+      }
+      else {
         this.clearSelectedDateArr();
-        this.selected.push(date);
-        this.selectedDateArr[row][col] = true;
-        this.start = col + 1;
-        this.selectedRow = row;
+        this.selectedRow = -1;
+      }
+
+      if (this.end > col && this.end < this.daysInMonthArr.length) {
+        this.selectedDateArr[row][this.end] = false;
+      }
+      this.end = col;
+    }
+    else {
+      for (let j = col; j >= this.start - 1; j--) {
+        this.selectedDateArr[row][j] = false;
       }
     }
+  }
 
-    onMouseArrOver(row: number, col: number, date: DateTime, roomNumber: string){
-      if (this.start > -1 && !this.checkReservation1(roomNumber, date)){
+  openModalAndSelectRangeBoxForRoomArr(view: ReservationView, item: any, row: number, col: number, date: DateTime) {
+    if (this.start > -1 && this.selectedRow === row && !this.checkReservationInRange(row, this.start, col + 1, item)) {
+      this.end = col;
+      this.reservationForm.name = '';
+      this.reservationForm = {} as ReservationForm;
+      this.reservationForm.roomNumber = view.room.roomNumber;
+      this.reservationForm.capacity = view.room.roomCapacity.capacity;
+      this.reservationForm.status = view.room.status;
+      this.reservationForm.startDate = item[this.start - 1];
+      this.reservationForm.endDate = item[col];
+      this.reservationForm.view = view;
+      this.showModal = true;
+      //const diff = DateTime.local(res.endDate.year, res.endDate.month, res.endDate.day).
+      //diff(DateTime.local(res.startDate.year, res.startDate.month, res.startDate.day), 'days').days + 1;
 
-          if ( this.selectedRow === row){
-            for(let i = this.start; i <= col; i++){
-                this.selectedDateArr[row][col]= true;
-            }
-          }
-          else{
-            this.clearSelectedDateArr();
-            this.selectedRow = -1;
-          }
-
-          if (this.end > col && this.end < this.daysInMonthArr.length){
-            this.selectedDateArr[row][this.end]= false;
-          }
-          this.end = col;
-        }
-        else{
-          for(let j = col; j>= this.start - 1; j--){
-            this.selectedDateArr[row][j]= false;
-          }
-        }
     }
-
-    openModalAndSelectRangeBoxForRoomArr(view: ReservationView, item: any, row: number, col: number, date: DateTime){
-      if (this.start > -1 && this.selectedRow === row &&  !this.checkReservationInRange(row, this.start, col + 1)){
-        this.end = col;
-        if (item[this.start -1]){
-            alert('Hello')
-            this.reservationForm.name = '';
-            this.reservationForm = {} as ReservationForm;
-            this.reservationForm.roomNumber = view.room.roomNumber;
-            this.reservationForm.capacity = view.room.roomCapacity.capacity;
-            this.reservationForm.status = view.room.status;
-            this.reservationForm.startDate = this.selected[0];
-            this.reservationForm.endDate = this.selected[this.selected.length -1];
-            this.reservationForm.view = view;
-            this.showModal = true;
-            //const diff = DateTime.local(res.endDate.year, res.endDate.month, res.endDate.day).
-            //diff(DateTime.local(res.startDate.year, res.startDate.month, res.startDate.day), 'days').days + 1;
-        }
-      }
+    else {
+      this.focusSearch();
     }
+  }
 
-    updateReservedSavedFromReservation(){
-      for(let m = 0; m < this.reservationMonths.length; m++){
-        let currMonthDate: DateTime = this.pageSelectedDate.plus({month:m});
-        let currMonthReservations = this.reservations.filter(f => f.startDate.year === currMonthDate.year &&
-          f.startDate.monthShort === currMonthDate.monthShort) as Reservation[];
-          if (currMonthReservations[0]){
-            currMonthReservations.forEach(item => {
-            for(let i = 0; i < this.reservationView.length; i++){
-              for(let j= 0; j < this.reservationView[i].dayDates.length; j++){
-                if (this.reservationView[i].room.roomNumber === item.reservedRoom.roomNumber && this.reservationView[i].dayDates[j].year === item.endDate.year &&
-                  this.reservationView[i].dayDates[j].month === item.endDate.month && this.reservationView[i].dayDates[j].day === item.endDate.day
-                ){
-                  this.reservationView[i].reservationSaved[j] = true;
+  updateReservedSavedFromReservation() {
+    for (let m = 0; m < this.reservationMonths.length; m++) {
+      let currMonthDate: DateTime = this.pageSelectedDate.plus({ month: m });
+      let currMonthReservations = this.reservations.filter(f => f.startDate.year === currMonthDate.year &&
+        f.startDate.monthShort === currMonthDate.monthShort) as Reservation[];
+      if (currMonthReservations[0]) {
+        currMonthReservations.forEach(item => {
+          for (let i = 0; i < this.reservationView.length; i++) {
+            for (let j = 0; j < this.reservationView[i].dayDates.length; j++) {
+              if (this.reservationView[i].room.roomNumber === item.reservedRoom.roomNumber && this.reservationView[i].dayDates[j].year === item.endDate.year &&
+                this.reservationView[i].dayDates[j].month === item.endDate.month && this.reservationView[i].dayDates[j].day === item.endDate.day
+              ) {
+                this.reservationView[i].reservationSaved[j] = true;
+              }
+              if (this.reservationView[i].room.roomNumber === item.reservedRoom.roomNumber && this.reservationView[i].dayDates[j].year === item.startDate.year &&
+                this.reservationView[i].dayDates[j].month === item.startDate.month && this.reservationView[i].dayDates[j].day === item.startDate.day
+              ) {
+                this.reservationView[i].reservationStartSaved[j] = true;
+                if (item.startDate === item.endDate) {
+                  this.reservationView[i].reservationName[j] = item.name;
                 }
-                if(this.reservationView[i].room.roomNumber === item.reservedRoom.roomNumber && this.reservationView[i].dayDates[j].year === item.startDate.year &&
-                  this.reservationView[i].dayDates[j].month === item.startDate.month && this.reservationView[i].dayDates[j].day === item.startDate.day
-                ){
-                  this.reservationView[i].reservationStartSaved[j] = true;
-                  if (item.startDate === item.endDate){
-                    this.reservationView[i].reservationName[j] = item.name;
-                  }
-                  else{
-                    this.reservationView[i].reservationName[j + 1] = item.name;
-                  }
+                else {
+                  this.reservationView[i].reservationName[j + 1] = item.name;
                 }
               }
             }
-          });
-        }
-      }
-    }
-
-    checkReservationInRange(row: number,start: number, end: number):boolean{
-      for(let j = start; j <= end; j++ ){
-        if (this.checkReservation(this.reservationView[row].room.roomNumber, j)){
-          return true;
-        }
-      } date: DateTime
-      return false;
-    }
-
-    deleteReservation(view: ReservationView, date: DateTime, row: number){
-      let res = this.reservations.find(f => f.reservedRoom.roomNumber === view.room.roomNumber && f.startDate <= date && date <= f.endDate
-      ) as Reservation;
-      let newRes: Reservation[] = [];
-      for(let j =0; j < this.reservations.length; j++)
-      {
-        if (this.reservations[j].reservedRoom.roomNumber != res.reservedRoom.roomNumber){
-          newRes.push(this.reservations[j]);
-        }
-        else if (this.reservations[j].startDate != res.startDate &&
-          this.reservations[j].endDate != res.endDate
-        ){
-          newRes.push(this.reservations[j]);
-        }
-      }
-      this.reservations = newRes;
-      for(let i = 0; i < view.dayDates.length; i++){
-        if (view.room.roomNumber === res.reservedRoom.roomNumber && view.dayDates[i].year === res.endDate.year && view.dayDates[i].month === res.endDate.month &&
-          view.dayDates[i].day === res.endDate.day
-        ){
-          view.reservationSaved[i] = false;
-        }
-        if (view.room.roomNumber === res.reservedRoom.roomNumber && view.dayDates[i].year === res.startDate.year && view.dayDates[i].month === res.startDate.month &&
-          view.dayDates[i].day === res.startDate.day){
-          view.reservationStartSaved[i] = false;
-          if ( res.startDate === res.endDate){
-            view.reservationName[i] = '';
           }
-          else{
-            view.reservationName[i + 1] = '';
-          }
-        }
-      }
-      this.clearSelectedDateArr();
-      for(let j = res.startDate.day; j< res.endDate.day; j++){
-          this.selectedDateArr[row][j] = false;
-      }
-      view.reservationSaved[res.endDate.day - 1] = false;
-      view.reservationStartSaved[res.startDate.day -1] = false;
-      if (res.days.length > 1){
-        view.reservationName[res.startDate.day] = '';
-      }
-      else{
-        view.reservationName[res.startDate.day -1] = '';
+        });
       }
     }
+  }
 
-    closeModal = (): void => {
-      this.showModal = false;
-    }
-
-    checkReservationStart(roomNumber:string, date: DateTime):boolean{
-      if (this.reservations.filter(f => f.reservedRoom.roomNumber === roomNumber && (
-        f.startDate === date))){
-       return true;
-     }
-     return false;
-
-    }
-
-    checkReservation1(roomNumber: string, date: DateTime):boolean{
-      if (this.reservations.find(f => f.reservedRoom.roomNumber === roomNumber &&
-         f.startDate <= date && date <= f.endDate)){
+  checkReservationInRange(row: number, start: number, end: number, dates: any[]): boolean {
+    for (let j = start; j <= end; j++) {
+      if (this.checkReservation1(this.reservationView[row].room.roomNumber, dates[j - 1])) {
         return true;
       }
-      return false;
-    }
+    } date: DateTime
+    return false;
+  }
 
-    checkReservation(roomNumber: string, day: number):boolean{
-      if (this.reservations.filter(f => f.reservedRoom.roomNumber === roomNumber &&
-         f.startDate.monthShort === this.pageSelectedDate.monthShort &&
-        f.startDate.year === this.pageSelectedDate.year).flatMap(fm => fm.days).find(d => d === day)){
-        return true;
+  deleteReservation(view: ReservationView, date: DateTime, row: number) {
+    let res = this.reservations.find(f => f.reservedRoom.roomNumber === view.room.roomNumber && f.startDate <= date && date <= f.endDate
+    ) as Reservation;
+    let newRes: Reservation[] = [];
+    for (let j = 0; j < this.reservations.length; j++) {
+      if (this.reservations[j].reservedRoom.roomNumber != res.reservedRoom.roomNumber) {
+        newRes.push(this.reservations[j]);
       }
-      return false;
+      else if (this.reservations[j].startDate != res.startDate &&
+        this.reservations[j].endDate != res.endDate
+      ) {
+        newRes.push(this.reservations[j]);
+      }
     }
-
-    goToPreviousMonth(){
-      this.reservationView = [] as ReservationView[];
-      this.pageSelectedDate = this.pageSelectedDate.minus({months: 1})
-      this.daysInMonth = this.pageSelectedDate.daysInMonth!;
-      this.daysInMonthArr = this.hotelService.getDaysInMonth(this.pageSelectedDate);
-      this.reservationView = this.hotelService.getReservationView(this.pageSelectedDate);
-      this.clearSelectedDateArr();
-      this.updateReservedSavedFromReservation();
-    }
-
-    goToNextMonth(){
-      this.reservationView = [] as ReservationView[];
-      this.pageSelectedDate = this.pageSelectedDate.plus({months: 1});
-      this.daysInMonth = this.pageSelectedDate.daysInMonth!;
-      this.daysInMonthArr = this.hotelService.getDaysInMonth(this.pageSelectedDate);
-      this.reservationView = this.hotelService.getReservationView(this.pageSelectedDate);
-      this.clearSelectedDateArr();
-      this.updateReservedSavedFromReservation();
-    }
-
-    public clearSelectedDateArr(){
-      for(let i = 0; i < this.reservationView.length;i++){
-        this.selectedDateArr[i] = [];
-        for(let j = 0; j< this.daysInMonthArr.length; j++){
-          this.selectedDateArr[i][j] = false;
+    this.reservations = newRes;
+    for (let i = 0; i < view.dayDates.length; i++) {
+      if (view.room.roomNumber === res.reservedRoom.roomNumber && view.dayDates[i].year === res.endDate.year && view.dayDates[i].month === res.endDate.month &&
+        view.dayDates[i].day === res.endDate.day
+      ) {
+        view.reservationSaved[i] = false;
+      }
+      if (view.room.roomNumber === res.reservedRoom.roomNumber && view.dayDates[i].year === res.startDate.year && view.dayDates[i].month === res.startDate.month &&
+        view.dayDates[i].day === res.startDate.day) {
+        view.reservationStartSaved[i] = false;
+        if (res.startDate === res.endDate) {
+          view.reservationName[i] = '';
         }
-      }
-      this.selected = [];
-    }
-
-    public clearReservationSaved(){
-      for(let i = 0; i < this.reservationView.length;i++){
-        this.reservationSaved[i] = [];
-        for(let j = 0; j< this.daysInMonthArr.length; j++){
-          this.reservationSaved[i][j] = false;
+        else {
+          view.reservationName[i + 1] = '';
         }
       }
     }
+    this.clearSelectedDateArr();
+    for (let j = res.startDate.day; j < res.endDate.day; j++) {
+      this.selectedDateArr[row][j] = false;
+    }
+    view.reservationSaved[res.endDate.day - 1] = false;
+    view.reservationStartSaved[res.startDate.day - 1] = false;
+    if (res.days && res.days.length > 1) {
+      view.reservationName[res.startDate.day] = '';
+    }
+    else {
+      view.reservationName[res.startDate.day - 1] = '';
+    }
+  }
+
+  closeModal = (): void => {
+    this.showModal = false;
+  }
+
+  checkReservationStart(roomNumber: string, date: DateTime): boolean {
+    if (this.reservations.filter(f => f.reservedRoom.roomNumber === roomNumber && (
+      f.startDate === date))) {
+      return true;
+    }
+    return false;
+
+  }
+
+  checkReservation1(roomNumber: string, date: DateTime): boolean {
+    if (this.reservations.find(f => f.reservedRoom.roomNumber === roomNumber &&
+      f.startDate <= date && date <= f.endDate)) {
+      return true;
+    }
+    return false;
+  }
+
+  checkReservation(roomNumber: string, day: number): boolean {
+    if (this.reservations.filter(f => f.reservedRoom.roomNumber === roomNumber &&
+      f.startDate.monthShort === this.pageSelectedDate.monthShort &&
+      f.startDate.year === this.pageSelectedDate.year).flatMap(fm => fm.days).find(d => d === day)) {
+      return true;
+    }
+    return false;
+  }
+
+  goToPreviousMonth() {
+    this.reservationView = [] as ReservationView[];
+    this.pageSelectedDate = this.pageSelectedDate.minus({ months: 1 })
+    this.daysInMonth = this.pageSelectedDate.daysInMonth!;
+    this.daysInMonthArr = this.hotelService.getDaysInMonth(this.pageSelectedDate);
+    this.reservationView = this.hotelService.getReservationView(this.pageSelectedDate);
+    this.clearSelectedDateArr();
+    this.updateReservedSavedFromReservation();
+  }
+
+  goToNextMonth() {
+    this.reservationView = [] as ReservationView[];
+    this.pageSelectedDate = this.pageSelectedDate.plus({ months: 1 });
+    this.daysInMonth = this.pageSelectedDate.daysInMonth!;
+    this.daysInMonthArr = this.hotelService.getDaysInMonth(this.pageSelectedDate);
+    this.reservationView = this.hotelService.getReservationView(this.pageSelectedDate);
+    this.clearSelectedDateArr();
+    this.updateReservedSavedFromReservation();
+  }
+
+  public clearSelectedDateArr() {
+    for (let i = 0; i < this.reservationView.length; i++) {
+      this.selectedDateArr[i] = [];
+      for (let j = 0; j < this.daysInMonthArr.length; j++) {
+        this.selectedDateArr[i][j] = false;
+      }
+    }
+    this.selected = [];
+  }
+
+  public clearReservationSaved() {
+    for (let i = 0; i < this.reservationView.length; i++) {
+      this.reservationSaved[i] = [];
+      for (let j = 0; j < this.daysInMonthArr.length; j++) {
+        this.reservationSaved[i][j] = false;
+      }
+    }
+  }
 }
